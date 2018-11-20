17075693abca38bb1628e1ce39324272052c960d
Mohamed EL-Ghoneimy <ghoneishy@hotmail.com>
2018-11-21 00:11:58 +0200

    update restaurant info details

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoiZ2hvbmVpc2h5ODUiLCJhIjoiY2pvZm94OG00MDYzdjN4dDdkbWp4em14biJ9.7m0T3CT-qWxjm7uUlbLIVA',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
